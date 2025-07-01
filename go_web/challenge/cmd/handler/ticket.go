a6ebd0b2b8777c81ea296df4a02fde9d2d53f544
Raphael Borba <raphaelbsx@gmail.com>
2025-07-01 11:12:51 -0300

    feat: add util packages for help responses http

--- go_web/challenge/cmd/handler/ticket.go
+++ go_web/challenge/cmd/handler/ticket.go
@@ -1,13 +1,13 @@
 package handler
 
 import (
-	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
 
 	"github.com/RaphaelBorba/challenge_web/internal/ticket"
 	"github.com/RaphaelBorba/challenge_web/pkg/apperrors"
+	"github.com/RaphaelBorba/challenge_web/pkg/web/response"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -23,11 +23,10 @@ func (h *TicketHandler) GetAll() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tickets, err := h.svc.GetAll()
 		if err != nil {
-			http.Error(w, apperrors.ErrInternalError.Error(), http.StatusInternalServerError)
+			response.Error(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(tickets)
+		response.JSON(w, http.StatusOK, map[string]any{"data": tickets, "message": "ok"})
 	}
 }
 
@@ -36,24 +35,23 @@ func (h *TicketHandler) GetByID() http.HandlerFunc {
 		idStr := chi.URLParam(r, "ticketId")
 		id, err := strconv.Atoi(idStr)
 		if err != nil {
-			http.Error(w, apperrors.ErrValidation.Error(), http.StatusBadRequest)
+			response.Error(w, http.StatusBadRequest, apperrors.ErrValidation.Error())
 			return
 		}
 
-		t, err := h.svc.GetById(id)
+		tkt, err := h.svc.GetById(id)
 		if err != nil {
 			switch {
 			case errors.Is(err, apperrors.ErrNotFound):
-				http.Error(w, err.Error(), http.StatusNotFound)
+				response.Error(w, http.StatusNotFound, err.Error())
 			case errors.Is(err, apperrors.ErrValidation):
-				http.Error(w, err.Error(), http.StatusBadRequest)
+				response.Error(w, http.StatusBadRequest, err.Error())
 			default:
-				http.Error(w, apperrors.ErrInternalError.Error(), http.StatusInternalServerError)
+				response.Error(w, http.StatusInternalServerError, apperrors.ErrInternalError.Error())
 			}
 			return
 		}
 
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(t)
+		response.JSON(w, http.StatusOK, map[string]any{"data": tkt, "message": "ok"})
 	}
 }
